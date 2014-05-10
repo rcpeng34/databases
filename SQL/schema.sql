eabcdd047cb4492097fca8db23aba212eb5bb528
Hack Reactor Students <rcpeng@gmail.com>
2014-05-09 21:14:01 -0700

    Adds functions to create tables

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,10 +1,40 @@
-CREATE DATABASE chat;
+#CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE messages;
+
+DROP TABLE users;
+
+DROP TABLE chatroom;
+
+DROP TABLE friends;
+
 CREATE TABLE messages (
- /* Describe your table here.*/
+  messageId int,
+  createdAt datetime,
+  messageText varchar(1000),
+  messageUserId int,
+  messageRoomId int
 );
+
+CREATE TABLE users (
+  userId int,
+  userName varchar(50)
+);
+
+
+CREATE TABLE chatroom (
+  roomId int,
+  roomName varchar(50)
+);
+
+CREATE TABLE friends (
+  userId int,
+  friendId int
+);
+
+
 
 /* You can also create more tables, if you need them... */
 
